225482c268a72e0686e7aa44bfbe33a4d95fcb49
dat <mirindano111@gmail.com>
2026-04-11 17:11:50 +0700

    Enhance queue processing in QueueTask; reduce delay to 5 seconds and add logging for user entry notifications

--- src/main/java/com/example/App/service/QueueTask.java
+++ src/main/java/com/example/App/service/QueueTask.java
@@ -2,17 +2,21 @@ package com.example.App.service;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 import lombok.RequiredArgsConstructor;
 
 // src/main/java/com/example/App/service/QueueTask.java
 @Component
 @RequiredArgsConstructor
+@Slf4j
+
 public class QueueTask {
     private final WaitingRoomService waitingRoomService;
 
-    @Scheduled(fixedDelay = 20000) // 10 giây thả người mới vào 1 lần
-    public void processQueue() {
-        waitingRoomService.allowUsersIn();
-    }
+    @Scheduled(fixedDelay = 5000) 
+public void processQueue() {
+    log.info("--- ĐANG MỜI NGƯỜI VÀO PHÒNG... ---"); // Thêm log này
+    waitingRoomService.allowUsersIn();
+}
 }
